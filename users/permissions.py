0c85a2a9998bfebde45f7c9de1eb5545650af7bc
wagner840 <wagnerlevel2@gmail.com>
2023-07-12 14:18:56 -0300

    Exclude Commentaries

--- users/permissions.py
+++ users/permissions.py
@@ -36,10 +36,6 @@ class IsSelfOrAdminReview(permissions.BasePermission):
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
         if request.method in permissions.SAFE_METHODS:
             return True
-
-        # Write permissions are only allowed to the owner of the review.
         return obj.user == request.user
